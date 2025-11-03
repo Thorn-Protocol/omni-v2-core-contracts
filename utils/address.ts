e55e817e759b93e7010a68b6bba69bb459dd64f4
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2025-11-03 10:20:06 +0700

    add address offchain vault

--- utils/address.ts
+++ utils/address.ts
@@ -2,5 +2,6 @@ export const addresses = {
   base: {
     usdc: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
     wasabiUSDCERC4626: "0x1C4a802FD6B591BB71dAA01D8335e43719048B24",
+    offChain: "0x00aa576bfa5f75BC6C651e8Cb587dD78b287040A",
   },
 };
